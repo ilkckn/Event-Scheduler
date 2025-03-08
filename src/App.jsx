35d1498e6d2c28784a9c15e4c000769617c26395
Abdulwahed Bin Mothana <abdulwahed.mothana@yahoo.com>
2025-03-09 00:25:54 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import EventContextProvider from "./context/EventContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./components/Login";
 import Home from "./pages/Home";
 import SignIn from "./components/SignIn";
-
+import DetailPage from "./pages/DetailsPage";
+import { useOutlet } from "react-router-dom";// rout
 function App() {
   return (
     <EventContextProvider>
       <div className="App w-full min-h-[100vh]">
         <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<SignIn />} />
+          {/* Protected Routes */}
           <Route element={<ProtectedRoute />}>
             <Route path="/home/:id/:name" element={<Home />} />
+            <Route path="/detail/:eventid" element={<DetailPage />} />    //DetailPage
           </Route>
+          {/* Protected Routes End */}
         </Routes>
       </div>
     </EventContextProvider>
   );
 }
-
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { EventContext } from "../context/EventContext";
+import { Link } from "react-router-dom";  ////for link 
 
 function Home() {
   console.log("Home component rendered");
   const { handleLogout } = useContext(EventContext);
   const { id, name } = useParams();
-
+  const eventid ="1";
   useEffect(() => {
     console.log("User ID:", id);
     console.log("User Name:", name);
@@ -15,7 +16,9 @@ function Home() {
   return (
     <div className="w-full flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold text-center mt-10">Home</h1>
-
+      <div>
+                
+              </div>
       {id && name ? (
         <p className="text-lg mt-4">
           Welcome,{" "}
@@ -34,7 +37,13 @@ function Home() {
       >
         Logout
       </button>
+      <Link to={`/detail/${eventid}`} className="text-blue-500 underline">
+          Details
+        </Link>
+
+      
     </div>
+
   );
 }
 
